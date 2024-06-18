f17f88cf7797cf2e9a9c764aea00ca57068160d0
Naveen <naveennandhi143@gmail.com>
2024-06-18 15:18:59 +0530

    add userdetails controller

--- src/main/java/com/_SoftTche/SpringCICDdemo/controller/Welcome.java
+++ src/main/java/com/_SoftTche/SpringCICDdemo/controller/Welcome.java
@@ -3,10 +3,18 @@ package com._SoftTche.SpringCICDdemo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class Welcome {
+    List<String> arr= new ArrayList<>();
     @GetMapping("/welcome")
     public String message(){
         return "welcome to your CICD Demo";
+    }
+    @GetMapping("/userDetails")
+    public String userDetails(){
+        return "add your name and address";
     }
 }
